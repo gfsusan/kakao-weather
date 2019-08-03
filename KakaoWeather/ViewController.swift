938dcfbc0812b1b4f5bcfdd25bafaa0cb93d7142
Susan Kim <suzenyada@gmail.com>
2019-08-03 18:43:57 +0900

    Initial Commit

---
+++ KakaoWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KakaoWeather
+//
+//  Created by Susan Kim on 03/08/2019.
+//  Copyright © 2019 Susan Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
